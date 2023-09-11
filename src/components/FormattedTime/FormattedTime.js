c3e9bceb3e3b39f39eebedfee9401f4abe658c20
Maciej Kicinski <maciejkicinski@MacBook-Air-Maciej.local>
2023-09-11 17:02:41 +0200

    -adding FormattedTime component

---
+++ src/components/FormattedTime/FormattedTime.js
@@ -0,0 +1,22 @@
+import styles from "/FormattedTime.module.scss";
+
+const FormattedTime = ({ time }) => {
+  const formatTime = (milliseconds) => {
+    const millis = Math.floor(milliseconds % 1000);
+    const seconds = Math.floor((milliseconds / 1000) % 60);
+    const minutes = Math.floor((milliseconds / 1000 / 60) % 60);
+    const hours = Math.floor((milliseconds / 1000 / 60 / 60) % 24);
+
+    const time = [
+      hours.toString().padStart(2, "0"),
+      minutes.toString().padStart(2, "0"),
+      seconds.toString().padStart(2, "0"),
+    ].join(":");
+
+    return time + "." + millis;
+  };
+
+  return <div className={styles.component}>{formatTime(time)}</div>;
+};
+
+export default FormattedTime;
